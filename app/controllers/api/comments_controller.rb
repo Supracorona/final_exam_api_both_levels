0684198c3cae23c5daf7b785596c56ba916bf46c
Supracorona <dejan@visekruna.com>
2022-05-19 16:27:04 +0200

    http response spec for create comment, body and factory validation for comment model spec and comment to database in comments_controller added

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,4 +1,14 @@
 class Api::CommentsController < ApplicationController
+
   def create
+    Comment.create(body: comment_params)
+    render json: { message: 'The comment was created' }, status: 201
   end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
+
 end
---
+++ spec/requests/api/comment/create_spec.rb
@@ -0,0 +1,24 @@
+describe 'POST /api/comments', type: :request do
+
+  describe 'successfully' do
+
+    before do
+      post '/api/comments',
+           params: { comment: { body: 'This is the body of the comment' } }
+    end
+
+    it 'is expected to return a 201 response' do
+      expect(response).to have_http_status 201
+    end
+    
+    it 'is expected to return a message' do
+      expect(response_json['message']).to eq 'The comment was created'
+    end
+
+    it 'is expected to save the comment in the comments table' do
+      expect(Comment.count).to eq 1
+    end
+  
+  end
+
+end
