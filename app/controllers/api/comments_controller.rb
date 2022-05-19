87dfadc89b5ecfba0266b0f8d9d4eb74fa952ad5
Supracorona <dejan@visekruna.com>
2022-05-19 16:41:11 +0200

    article_id to comments for association and sad paths added

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,14 +1,23 @@
 class Api::CommentsController < ApplicationController
 
   def create
-    Comment.create(body: comment_params)
-    render json: { message: 'The comment was created' }, status: 201
+    comment = Comment.new(comment_params)
+    comment.save
+
+    if comment.persisted?
+      render json: { message: 'The comment was created' }, status: 201
+    elsif !comment[:body]
+      render json: { message: 'The comment cannot be empty' }, status: 422
+    else
+      render json: { message: 'Comments must be made on an article' }, status: 422
+    end
+  
   end
 
   private
 
   def comment_params
-    params.require(:comment).permit(:body)
+    params.require(:comment).permit(:article_id, :body)
   end
 
 end
--- spec/requests/api/comment/create_spec.rb
+++ spec/requests/api/comment/create_spec.rb
@@ -1,16 +1,21 @@
 describe 'POST /api/comments', type: :request do
 
+  subject { response }
+
+  let!(:article) { create(:article) }
+
+
   describe 'successfully' do
 
     before do
       post '/api/comments',
-           params: { comment: { body: 'This is the body of the comment' } }
+           params: { comment: { article_id: article.id, body: 'This is the body of the comment' } }
     end
 
     it 'is expected to return a 201 response' do
       expect(response).to have_http_status 201
     end
-    
+
     it 'is expected to return a message' do
       expect(response_json['message']).to eq 'The comment was created'
     end
@@ -18,7 +23,44 @@ describe 'POST /api/comments', type: :request do
     it 'is expected to save the comment in the comments table' do
       expect(Comment.count).to eq 1
     end
-  
+
   end
+
+
+  describe 'unsuccessfully: with no content in comment' do
+
+    before do
+      post '/api/comments',
+           params: { comment: { article_id: article.id } }
+    end
+
+    it 'is expected to return a 422 response' do
+      expect(response).to have_http_status 422
+    end
+
+    it 'is expected to return an error message' do
+      expect(response_json['message']).to eq 'The comment cannot be empty'
+    end
+
+  end
+
+
+  describe 'unsuccessfully: with no associated article' do
+
+    before do
+      post '/api/comments',
+           params: { comment: { body: 'I am a comment body' } }
+    end
+
+    it 'is expected to return a 422 response' do
+      expect(response).to have_http_status 422
+    end
+
+    it 'is expected to return an error message' do
+      expect(response_json['message']).to eq 'Comments must be made on an article'
+    end
+
+  end
+
 
 end
